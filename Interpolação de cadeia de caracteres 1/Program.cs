c0b7e64be760a56ea05913ad433d963a2d6fa78c
codeflavio <flaviopvieira@hotmail.com>
2020-12-31 09:50:31 -0300

    Interpolação

---
+++ Interpolação de cadeia de caracteres 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Interpolação_de_cadeia_de_caracteres_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aFriend = "Bill";
+            Console.WriteLine("Olá !" + aFriend);
+        }
+    }
+}
---
+++ Interpolação de cadeia de caracteres.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hello_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aFriend = "Bill";
+            Console.WriteLine(aFriend);
+
+        }
+    }
+}
